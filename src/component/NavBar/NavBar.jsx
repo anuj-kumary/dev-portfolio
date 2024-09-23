caf5c7da8d2083f854f02076a5beacae0b4f5517
Anujkumar Yadav <anujkumaryadav@Anujkumars-MacBook-Pro.local>
2024-09-23 14:10:08 +0530

    removed unwated file

---
+++ src/component/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import "../Navbar/NavBar.css"
+export const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h3>{'<Anujkumar Yadav />'}</h3>
+      <button className="lets-talk-btn"> Let&apos;s talk</button>
+    </nav>
+  )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { NavBar } from './component/NavBar/NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <NavBar />
     <App />
   </StrictMode>,
 )
