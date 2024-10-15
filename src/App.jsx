4a480aa9e33d0e132d2ff6fbd4bcec78017e6c48
anuj-kumary <anujf0510@gmail.com>
2024-10-15 11:01:51 +0530

    Added recent works component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { HeroSection } from './component/HeroSection/HeroSection'
+import { Works } from './component/Works/Works'
 
 function App() {
 
   return (
     <>
     <HeroSection />
+    <Works />
     </>
   )
 }
---
+++ src/component/Works/Works.jsx
@@ -0,0 +1,12 @@
+import "./Works.css"
+export const Works =()=> {
+  return (
+    <div className="container">
+    <div className="row">
+    <h3 className="heading">Recent <span className="text-gradient">Works</span></h3>
+    <p className="sub-heading">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+    </div>
+     
+    </div>
+  )
+}
