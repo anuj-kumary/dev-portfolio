e07719ede238033b2abce68221e9546803168335
anuj-kumary <anujf0510@gmail.com>
2024-11-23 11:10:29 +0530

    Fix content for showcase work section

--- src/component/Works/Works.jsx
+++ src/component/Works/Works.jsx
@@ -1,13 +1,19 @@
-import { PortfolioWork } from "../../common/Card/Card"
-import "./Works.css"
-export const Works =()=> {
+import { PortfolioWork } from "../../common/Card/Card";
+import "./Works.css";
+export const Works = () => {
   return (
     <div className="container">
-    <div className="row">
-    <h3 className="heading">Recent <span className="text-gradient">Works</span></h3>
-    <p className="sub-heading">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+      <div className="row">
+        <h3 className="heading">
+          Recent <span className="text-gradient">Works</span>
+        </h3>
+        <p className="sub-heading">
+          Here are some of the projects I’ve worked on recently, showcasing my
+          skills in design, development, and problem-solving. Each project
+          represents a step forward in my journey as a developer.
+        </p>
+      </div>
+      <PortfolioWork />
     </div>
-     <PortfolioWork />
-    </div>
-  )
-}
+  );
+};
